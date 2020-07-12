2662f4e260868c3fab3255fd9e512bb2682f2e78
jai10w <67532821+jai10w@users.noreply.github.com>
2020-07-12 21:06:38 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,25 +1,20 @@
-var fixedRect, movingRect;
+var car, wall;
+var speed, weight;
 
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(400, 100, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400, 800,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
+  createCanvas(1600,400);
 
-  movingRect.velocityY = -5;
-  fixedRect.velocityY = +5;
+  createSprite(400, 200, 50, 50);
+  car = createSprite(50, 200, 50, 50);
+  wall=createSprite(1500, 200, 60, height/2);
+
+  wall.background = (80, 80, 80);
+
+  speed=random(55, 90);
+  weight=random(400, 1500);
 }
 
 function draw() {
-  background(0,0,0);  
-
-  bounceOff(movingRect, fixedRect);
-
+  background(255,255,255);  
   drawSprites();
 }
-
-
-
