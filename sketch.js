beb89142dc9b33c2bd33b326efe0acbee3048f36
jai10w <67532821+jai10w@users.noreply.github.com>
2020-07-12 21:16:24 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,20 +1,67 @@
-var car, wall;
-var speed, weight;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
-  createCanvas(1600,400);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  createSprite(400, 200, 50, 50);
-  car = createSprite(50, 200, 50, 50);
-  wall=createSprite(1500, 200, 60, height/2);
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-  wall.background = (80, 80, 80);
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-  speed=random(55, 90);
-  weight=random(400, 1500);
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
 }
+
 
 function draw() {
-  background(255,255,255);  
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
   drawSprites();
+ 
 }
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	var packageBody_options ={
+		restitution: 1.0
+	}
+    
+  }
+}
+
+
+
